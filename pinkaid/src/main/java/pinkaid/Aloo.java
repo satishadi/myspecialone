1ddf69d68d1a27fc9940aba8e8195e4b73a9ec00
satishadi <satishadi125@gamil.com>
2022-10-16 12:39:58 +0530

    my second commit aloo and broker

---
+++ pinkaid/src/main/java/pinkaid/Aloo.java
@@ -0,0 +1,11 @@
+package pinkaid;
+
+public class Aloo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=90;
+System.out.println("satish local");
+	}
+
+}
---
+++ pinkaid/src/main/java/pinkaid/Broker.java
@@ -0,0 +1,11 @@
+package pinkaid;
+
+public class Broker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int d=98;
+System.out.println("satish be it a " + d);
+	}
+
+}
