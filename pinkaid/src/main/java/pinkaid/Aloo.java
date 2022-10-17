f6b800e3925c7aca10471a5654fecf83098bb5e4
satishadi <satishadi125@gamil.com>
2022-10-17 12:36:04 +0530

    hsakjhfh

--- pinkaid/src/main/java/pinkaid/Aloo.java
+++ pinkaid/src/main/java/pinkaid/Aloo.java
@@ -4,8 +4,8 @@ public class Aloo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=90;
-System.out.println("satish local"+a);
+int h=90;
+System.out.println("satish local"+h);
 	}
 
 }
