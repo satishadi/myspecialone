5db84068c1173df88fba9bb46528650106befc34
satishadi <satishadi125@gamil.com>
2022-10-17 11:40:45 +0530

    my commit may be third

---
+++ pinkaid/src/main/java/pinkaid/babe.java
@@ -0,0 +1,11 @@
+package pinkaid;
+
+public class babe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int h = 92;
+System.out.println("satish is all at once"+ h);
+	}
+
+}
