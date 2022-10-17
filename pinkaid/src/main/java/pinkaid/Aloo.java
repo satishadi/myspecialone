7cc8aeed7eb47f644cbe2545acb5a29e29da699d
satishadi <satishadi125@gamil.com>
2022-10-17 12:34:13 +0530

    commit five

--- pinkaid/src/main/java/pinkaid/Aloo.java
+++ pinkaid/src/main/java/pinkaid/Aloo.java
@@ -5,7 +5,7 @@ public class Aloo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a=90;
-System.out.println("satish local");
+System.out.println("satish local"+a);
 	}
 
 }
